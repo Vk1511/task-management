33e4ceb7a5817cdb558a49da7f6749e693c513c3
Vishw Kadu <vishwkadu@Vishws-MacBook-Air.local>
2024-05-25 13:03:48 +0530

    added model to manage comments and shared task

--- backend/task_tickets/models.py
+++ backend/task_tickets/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from jwt_auth.models import Users
 
+"""
+    Model Use: Manage User Task
+    Referred from: Users
+    Referred by: SharedTask, TaskComments
+"""
+
 
 class UserTask(models.Model):
     class TaskPriority(models.TextChoices):
@@ -34,9 +40,58 @@ class UserTask(models.Model):
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    # TODO: add one to manage to manage document
 
-    class Mets:
+    class Meta:
         unique_together = ("title", "created_by")
 
     def __str__(self):
-        return self.name
+        return f"Task {self.title} created by {self.created_by}."
+
+
+"""
+    Model Use: Manage shared task
+    Referred from: Users, UserTask
+    Referred by: None
+    Description: We can achive this feature by using one to one relationship in UserTask model but it will be difficult
+        to manage when it shared, who shared with whom. 
+        as we are allowing sharing of all public task(owner as well as authenticated user can share)
+"""
+
+
+class SharedTask(models.Model):
+    task_id = models.ForeignKey(UserTask, on_delete=models.CASCADE)
+    shared_with = models.ForeignKey(
+        Users, on_delete=models.CASCADE, related_name="shared_with_user"
+    )
+    shared_by = models.ForeignKey(
+        Users, on_delete=models.CASCADE, related_name="shared_by_user"
+    )
+    shared_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # ? User can share task only with one user
+        unique_together = ("task_id", "shared_with", "shared_by")
+
+    def __str__(self):
+        return f"Task shared with {self.shared_with} by {self.shared_by}"
+
+
+"""
+    Model Use: Manage Task comments
+    Referred from: Users, UserTask
+    Referred by: None
+"""
+
+
+class TaskComments(models.Model):
+    task_id = models.ForeignKey(UserTask, on_delete=models.CASCADE)
+    comment = models.TextField(blank=False)
+    comment_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+    comment_at = models.DateTimeField(auto_now=True)
+    comment_updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    # TODO: add one to manage to manage documents
+
+    def __str__(self):
+        return f"Task comment added by {self.comment_by}"
